93358c4fefb25f0aa38a166a6f03d04618cdee00
hkq-hub <383799072@qq.com>
2021-05-11 20:02:15 +0800

    提交5

--- HelloGit/src/main/java/com/crrc/Teacher.java
+++ HelloGit/src/main/java/com/crrc/Teacher.java
@@ -17,4 +17,9 @@ public class Teacher {
     public void setRemark(String remark) {
         this.remark = remark;
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
 }
