bc92cafb37614f8289f12000239b2f8955563581
hkq-hub <383799072@qq.com>
2021-05-11 19:47:53 +0800

    提交2

---
+++ HelloGit/src/main/java/com/crrc/Helllo.java
@@ -0,0 +1,15 @@
+package com.crrc;
+
+public class Helllo {
+
+    public String name;
+    public int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
