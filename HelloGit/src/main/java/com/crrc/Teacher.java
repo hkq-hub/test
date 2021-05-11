aba3f20d534a87a53668ac40afadffe900861e65
hkq-hub <383799072@qq.com>
2021-05-11 19:50:20 +0800

    提交4

---
+++ HelloGit/src/main/java/com/crrc/Teacher.java
@@ -0,0 +1,20 @@
+package com.crrc;
+
+public class Teacher {
+
+    public String name;
+    public int age;
+    public String remark;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
